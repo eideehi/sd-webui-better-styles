afcbdd46d430db3b33d66e1202ba09d63cba548e
eideehi <41231321+eideehi@users.noreply.github.com>
2023-07-08 14:20:23 +0900

    Refactor checkpoint readable

--- client-src/libs/store/checkpoint.ts
+++ client-src/libs/store/checkpoint.ts
@@ -2,24 +2,13 @@ import { readable } from "svelte/store";
 import { getElement } from "@/libs/util/dom";
 
 export const checkpoint = readable("", (set) => {
-  let element = getElement("#sd_checkpoint_hash");
-  const _getElement = () => {
-    if (element == null) {
-      element = getElement("#sd_checkpoint_hash");
+  let currentHash = "";
+  let timeout = window.setTimeout(function update() {
+    const hash = getElement("#sd_checkpoint_hash")?.title || "";
+    if (hash !== currentHash) {
+      set((currentHash = hash));
     }
-    return element;
-  };
-
-  let current: Nullable<string> = "";
-  const checkUpdate = () => {
-    const hash = _getElement()?.title;
-    if (hash === current) return;
-    set((current = hash) || "");
-  };
-
-  checkUpdate();
-  const interval = window.setInterval(checkUpdate, 250);
-  return () => {
-    window.clearInterval(interval);
-  };
+    timeout = window.setTimeout(update, 125);
+  }, 10);
+  return () => window.clearTimeout(timeout);
 });
